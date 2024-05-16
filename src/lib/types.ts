0b4ac832dbe07a92f461536511bb9ffe50b0fd6d
Hunter Johnston <johnstonhuntera@gmail.com>
2024-05-16 18:44:05 -0400

    initial commit

---
+++ src/lib/types.ts
@@ -0,0 +1,11 @@
+import type { ReadableBox } from '$lib/box/box.svelte.js';
+
+// eslint-disable-next-line ts/no-explicit-any
+export type FunctionArgs<Args extends any[] = any[], Return = void> = (...args: Args) => Return;
+
+export type Getter<T> = () => T;
+export type MaybeGetter<T> = T | Getter<T>;
+export type MaybeBoxOrGetter<T> = T | Getter<T> | ReadableBox<T>;
+export type BoxOrGetter<T> = Getter<T> | ReadableBox<T>;
+
+export type Expand<T> = T extends infer U ? { [K in keyof U]: U[K] } : never;
