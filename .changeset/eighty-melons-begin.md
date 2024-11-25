ac3f4382a6bd964ff9099cb7652e1cfd17501e5f
Hunter Johnston <johnstonhuntera@gmail.com>
2024-11-24 19:47:09 -0500

    changeset

---
+++ .changeset/eighty-melons-begin.md
@@ -0,0 +1,5 @@
+---
+"svelte-toolbelt": patch
+---
+
+return result of `setTimeout` from `afterSleep`
--- src/lib/utils/after-sleep.ts
+++ src/lib/utils/after-sleep.ts
@@ -2,5 +2,5 @@
  * A utility function that executes a callback after a specified number of milliseconds.
  */
 export function afterSleep(ms: number, cb: () => void) {
-	setTimeout(cb, ms);
+	return setTimeout(cb, ms);
 }
