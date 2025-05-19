0dbdffa4f68ca7aebe9ce275f0f518c1658e97c0
Hunter Johnston <johnstonhuntera@gmail.com>
2025-05-19 19:55:47 -0400

    doc

--- src/lib/utils/attach-ref.ts
+++ src/lib/utils/attach-ref.ts
@@ -3,6 +3,22 @@ import { createAttachmentKey } from "svelte/attachments";
 
 type RefSetter<T> = (v: T) => void;
 
+/**
+ * Creates a Svelte Attachment that attaches a DOM element to a ref.
+ * The ref can be either a WritableBox or a callback function.
+ *
+ * @param ref - Either a WritableBox to store the element in, or a callback function that receives the element
+ * @returns An object with a spreadable attachment key that should be spread onto the element
+ *
+ * @example
+ * // Using with WritableBox
+ * const ref = box<HTMLDivElement | null>(null);
+ * <div {...attachRef(ref)}>Content</div>
+ *
+ * @example
+ * // Using with callback
+ * <div {...attachRef((node) => myNode = node)}>Content</div>
+ */
 export function attachRef<T extends EventTarget = Element>(
 	ref: WritableBox<T | null> | RefSetter<T | null>
 ) {
